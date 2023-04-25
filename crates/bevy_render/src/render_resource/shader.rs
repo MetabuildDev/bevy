d5a6a0f9e826f95a33009fa60159e2911a5f738d
nomaterials <nomaterials@gmail.com>
2023-04-25 10:41:53 +0200

    Updating wgpu to 0.16, naga to 0.12

--- crates/bevy_render/src/render_resource/shader.rs
+++ crates/bevy_render/src/render_resource/shader.rs
@@ -121,7 +121,7 @@ impl ProcessedShader {
             // TODO: process macros here
             ProcessedShader::Wgsl(source) => naga::front::wgsl::parse_str(source)?,
             ProcessedShader::Glsl(source, shader_stage) => {
-                let mut parser = naga::front::glsl::Parser::default();
+                let mut parser = naga::front::glsl::Frontend::default();
                 parser
                     .parse(&naga::front::glsl::Options::from(*shader_stage), source)
                     .map_err(ShaderReflectError::GlslParse)?
@@ -136,7 +136,7 @@ impl ProcessedShader {
         };
         const CAPABILITIES: &[(Features, Capabilities)] = &[
             (Features::PUSH_CONSTANTS, Capabilities::PUSH_CONSTANT),
-            (Features::SHADER_FLOAT64, Capabilities::FLOAT64),
+            (Features::SHADER_F64, Capabilities::FLOAT64),
             (
                 Features::SHADER_PRIMITIVE_INDEX,
                 Capabilities::PRIMITIVE_INDEX,
